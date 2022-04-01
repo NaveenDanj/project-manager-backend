e40106ce3ab9c4f4dc276481ecb3cea26b6692d0
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-04-02 00:12:58 +0530

    added login and register endpoints

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserAuthController extends Controller
+{
+
+    public function Register(Request $request){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // email verification
+
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $user->createToken('default')->plainTextToken
+        ], 201);
+
+
+    }
+
+
+    public function Login(Request $request){
+
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+
+        return response()->json([
+            'message' => 'login success',
+            'user' => $user,
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ]);
+
+
+    }
+
+
+
+
+}
